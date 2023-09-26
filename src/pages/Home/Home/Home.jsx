594f59d95069f2cb4e1156c886938f69da0edffb
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-26 23:43:45 +0600

    services

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroBg from '../HeroBg/HeroBg';
 import Hero from '../Hero/Hero';
+import Services from '../Services/Services';
 
 export default function Home() {
      return (
           <div>
                <HeroBg />
                <Hero />
+               <Services />
           </div>
      )
 }
---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+//img 
+import faceTreatment from '../../../assets/icons/Group1373.png';
+import hairColor from '../../../assets/icons/Group1372.png';
+import skinCare from '../../../assets/icons/Group1374.png';
+
+
+
+const services = [
+     {
+          id: 1,
+          image: faceTreatment,
+          name: "Anti Age Face Treatment",
+          price: "199",
+          describe: "We craft stunning and amazing web UI, using a well drrafted UX to fit your product."
+     },
+     {
+          id: 2,
+          image: hairColor,
+          name: "Hair Color & Styleing",
+          price: "99",
+          describe: "Amazing flyers, social media posts and brand representations that would make your brand stand out."
+     },
+     {
+          id: 3,
+          image: skinCare,
+          name: "Skin Care Treatment",
+          price: "299",
+          describe: "With well written codes, we build amazing apps for all platforms, mobile and web apps in general."
+     },
+]
+
+export default function Services() {
+     return (
+          <div className='mt-44'>
+               <h1 className='text-[#111430] font-bold text-4xl block text-center'>
+                    Our Awesome <span className='text-primary'>Services</span>
+               </h1>
+          </div>
+     )
+}
