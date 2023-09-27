de3b20740c19f7d5308c04fc56ec408f6ad6e0cd
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-27 08:59:24 +0600

    services

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function ServiceCard({ service }) {
+     console.log(service)
+     const { image, name, price, describe } = service;
+
+     return (
+          <div className='flex flex-col items-center justify-center gap-2 w-96 h-80 hover:border border-black rounded-xl p-10 transition'>
+               <img src={image} alt="" className='w-20' />
+               <p className='text-xl font-semibold text-[#111430]'>{name}</p>
+               <p className='text-xl font-medium text-primary px-2'>${price}</p>
+               <p className='font-light text-base text-gray text-center h-auto'>{describe}</p>
+          </div>
+     )
+}
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import faceTreatment from '../../../assets/icons/Group1373.png';
 import hairColor from '../../../assets/icons/Group1372.png';
 import skinCare from '../../../assets/icons/Group1374.png';
+import ServiceCard from './ServiceCard';
 
 
 
@@ -33,10 +34,17 @@ const services = [
 
 export default function Services() {
      return (
-          <div className='bg-white py-32'>
+          <div className='bg-white px-32 py-32'>
                <h1 className='text-[#111430] font-bold text-4xl block text-center'>
                     Our Awesome <span className='text-primary'>Services</span>
                </h1>
+
+               <div className="grid grid-cols-3 gap-x-16 pt-20">
+                    {
+                         services.map(service => <ServiceCard key={service.id} service={service} />)
+                    }
+               </div>
+
           </div>
      )
 }
