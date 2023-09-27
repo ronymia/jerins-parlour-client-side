f9f5b50646fd4d2fe7da75ada0aee754b1e4eb2b
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-27 09:52:58 +0600

    portfolio

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import Services from '../Services/Services';
+import Portfolio from '../Portfolio/Portfolio';
 
 export default function Home() {
      return (
           <div className='bg-[#FFF8F5]'>
                <Hero />
                <Services />
+               <Portfolio />
           </div>
      )
 }
---
+++ src/pages/Home/Portfolio/Portfolio.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import img from "../../../assets/images/engin-akyurt-g-m8EDc4X6Q-unsplash 1.png";
+
+export default function Portfolio() {
+     return (
+          <div className="grid grid-cols-2 items-center justify-center gap-24 px-32 py-32">
+               <div className="">
+                    <img src={img} alt="" />
+               </div>
+
+               <div className="flex flex-col gap-6 items-start justify-between w-3/4 h-auto">
+                    <h1 className='text-4xl font-semibold text-[#2D2D2D] leading-normal'>Let us handle your <br /> <span className='text-primary'>screen Professionally</span>.</h1>
+                    <p className='text-sm font-light text-gray'>
+                         With well written codes, we build amazing apps for all platforms, mobile and web apps in general ipsum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum.
+                    </p>
+                    <div className="flex flex-col gap-5 w-full">
+                         <div className="flex flex-row items-center justify-between text-primary text-5xl font-semibold ">
+                              <p>500+</p>
+                              <p>16+</p>
+                         </div>
+                         <div className="flex flex-row items-center justify-between">
+                              <p>Happy Customer</p>
+                              <p>Total Service</p>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     )
+}
