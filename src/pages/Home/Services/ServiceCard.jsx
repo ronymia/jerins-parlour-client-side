afded15469725cdc8bd6aa4ed15bc95c39962175
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-10-21 23:55:56 +0600

    bug fixed

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -3,13 +3,14 @@ import { useNavigate } from 'react-router-dom';
 
 export default function ServiceCard({ service }) {
      const navigate = useNavigate();
+     console.log(service)
      const { _id, image, title, price, describe } = service;
 
      return (
           <div onClick={() => navigate(`/service/${_id}`)}
                className="flex flex-col items-center justify-center gap-2 w-96 h-80 shadow-xl rounded-xl p-10 transition cursor-pointer">
                <img src={image} alt="serive image" className='w-20' />
-               <p className="text-xl font-semibold text-[#111430]">{title}</p>
+               <p className="text-xl font-semibold text-[#111430]">{service.service}</p>
                <p className="text-xl font-medium text-primary px-2">${price}</p>
                <p className="font-light text-base text-gray text-center h-auto">{describe}</p>
           </div>
