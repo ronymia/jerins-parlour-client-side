0c8c342adac889b65098adf5ccd19ff7cbc93b59
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-11-06 23:46:43 +0600

    hero responsive

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-
+//hero img 
 import HeroBg from '../../../assets/images/beautiful-young-asian-woman-touching-her-clean-face-with-fresh-healthy-skin-isolated-white-wall-beauty-cosmetics-facial-treatment-concept 1.png';
 
 export default function Hero() {
      return (
-          <div className="grid grid-cols-2 justify-center items-center px-20 py-5 mb-10">
-               <div className=" flex flex-col items-start justify-center gap-y-6">
-                    <h1 className='text-5xl font-bold text-[#111430] leading-normal'>BEAUTY SALON <br /> FOR EVERY WOMEN</h1>
-                    <p className='text-[#666666] w-3/4'>
+          <section className="grid grid-cols-1 md:grid-cols-2 justify-center items-center md:px-20 px-6 py-5 mb-10">
+               {/* banner left side  */}
+               <div className="order-2 md:order-1 flex flex-col items-start justify-center gap-y-6">
+                    <h1 className='md:text-5xl text-3xl mt-4 md:mt-0 font-bold text-[#111430] leading-normal md:leading-normal'>BEAUTY SALON <br /> FOR EVERY WOMEN</h1>
+                    <p className='text-[#666666] md:w-3/4 w-full'>
                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum duis laoreet maecenas. Feugiat
                     </p>
 
@@ -16,11 +16,13 @@ export default function Hero() {
                     >Get an Appointment</button>
 
                </div>
-               <div className="flex items-center justify-center">
+
+               {/* banner right side  */}
+               <div className="order-1 md:order-2 flex items-center justify-center">
                     <img src={HeroBg} alt=""
                          className='w-[450px]'
                     />
                </div>
-          </div>
+          </section>
      )
 }
