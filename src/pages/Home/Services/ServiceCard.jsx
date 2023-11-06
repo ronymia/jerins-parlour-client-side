5817cb6cdff3f5033e2cd06cd0d5c15e59c9c655
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-11-06 23:58:47 +0600

    service responsive

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -7,11 +7,11 @@ export default function ServiceCard({ service }) {
 
      return (
           <div onClick={() => navigate(`/service/${_id}`)}
-               className="flex flex-col items-center justify-center gap-2 w-96 h-80 shadow-xl rounded-xl p-10 transition cursor-pointer">
+               className="flex flex-col items-center justify-center gap-2 max-w-xs md:w-96 h-80 shadow-xl rounded-xl p-10 transition cursor-pointer">
                <img src={image} alt="serive image" className='w-20' />
                <p className="text-xl font-semibold text-[#111430]">{service.service}</p>
                <p className="text-xl font-medium text-primary px-2">${price}</p>
-               <p className="font-light text-base text-gray text-center h-auto">{describe}</p>
+               <p className="text-sm md:text-base font-light text-gray text-center h-auto">{describe}</p>
           </div>
      )
 }
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -29,12 +29,12 @@ export default function Services() {
      const { data: services = [] } = useQuery(getServices());
 
      return (
-          <div className="bg-white px-20 pt-20 pb-32 w-full flex flex-col items-center justify-center">
-               <h1 className="text-[#111430] font-bold text-4xl">
+          <section className="bg-white md:px-20 px-6 pt-20 pb-32 w-full flex flex-col items-center justify-center">
+               <h1 className="text-[#111430] font-bold text-4xl text-center">
                     Our Awesome <span className="text-primary">Services</span>
                </h1>
 
-               <div className="grid grid-cols-3 gap-x-16 pt-20">
+               <div className="grid grid-cols-1 md:grid-cols-3 gap-y-6 md:gap-x-16 pt-20">
                     {
                          services.map(service => <ServiceCard key={service._id} service={service} />)
                     }
@@ -43,6 +43,6 @@ export default function Services() {
                {/* <button type="button"
                     className='h-12 px-6 bg-primary text-white font-medium rounded-md mt-10'
                >Explore more</button> */}
-          </div>
+          </section>
      )
 }
