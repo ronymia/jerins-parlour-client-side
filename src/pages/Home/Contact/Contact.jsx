fca4c817462c9a7709993a3bc54ab7e1018b097f
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-11-12 22:30:48 +0600

    mobile responsive

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -12,16 +12,16 @@ export default function Contact() {
      }
 
      return (
-          <div className="py-20 flex flex-col items-center justify-center">
-               <h3 className="text-4xl text-[#2D2D2D] font-bold leading-normal text-center mb-20">
+          <div className="py-20 flex flex-col items-center justify-center px-6">
+               <h3 className="text-3xl md:text-4xl text-[#2D2D2D] font-bold leading-normal md:leading-normal text-center mb-20">
                     Let us handle your <br />
-                    project, professionally.
+                    project, professionally
                </h3>
 
                <form onSubmit={handleSubmit(onSubmit)}
-                    className="w-[750px] flex flex-col items-center justify-center gap-6"
+                    className="w-full md:w-[750px] flex flex-col items-center justify-center gap-6"
                >
-                    <div className="grid grid-cols-2 gap-6 w-full">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
                          <input type="text"
                               className="h-14 px-5 py-4 text-base text-black placeholder:text-sm placeholder:text-[#707070] placeholder:capitalize focus:outline-none bg-white rounded-md"
                               placeholder="first name"
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
                <Services />
                <Portfolio />
                <Testimonial />
-               {/* <Contact /> */}
+               <Contact />
           </div>
      )
 }
