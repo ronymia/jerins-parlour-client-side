3f33617d48879e5acade58917607a50139f09047
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-11-12 22:25:19 +0600

    testimnial

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
                <Hero />
                <Services />
                <Portfolio />
-               {/* <Testimonial /> */}
+               <Testimonial />
                {/* <Contact /> */}
           </div>
      )
